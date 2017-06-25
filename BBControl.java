3613a4e2a76b4fbc3d853fa219ad36e334e2b061
Vanessa72 <vanessa_schnellmann@hotmail.de>
2017-06-25 09:37:59 +0200

    Update BBControl.java

--- BBControl.java
+++ BBControl.java
@@ -1,6 +1,13 @@
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class BBController implements ActionListener {
+public class BBControl implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 	
 
