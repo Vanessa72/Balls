1da5559760ef91505955d57afeaa59e1b322d927
Vanessa72 <vanessa_schnellmann@hotmail.de>
2017-06-27 19:55:58 +0200

    Update BBModel.java

--- BBModel.java
+++ BBModel.java
@@ -1,4 +1,29 @@
 
 public class BBModel {
 
+	double height, velocity;
+//  double thrust;
+	final static double g = 9.81;
+	
+	public void reset(double height)
+	{
+		this.height = height;
+		this.velocity = 0;
+	//  this.thrust = 0;		
+	}
+	
+	public boolean tick(double dt)
+	{
+		height += dt * velocity;
+	//	double acceleration = thrust - g;
+	//	velocity += dt * acceleration;
+		
+		if(height < 0)
+		{
+			height = 0;
+			return false;
+		}
+		else
+			return true;
+	}
 }
