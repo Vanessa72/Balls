ebfb90c444e6d9939db9f71ffb77995b1443d5f6
Vanessa72 <vanessa_schnellmann@hotmail.de>
2017-06-29 19:59:20 +0200

    Update BBView.java

--- BBView.java
+++ BBView.java
@@ -5,24 +5,28 @@ import javax.swing.JPanel;
 
 public class BBView extends JPanel {
 
+	BBModel model;
+	
 	double height;
 	double velocity;
 	double visibleHeight;
 	
 	public BBView(double visibleHeight)
 	{
+		model = new BBModel();
 		
 		this.visibleHeight = visibleHeight;
 		
 	}
 	
-	public void paintComponent(Graphics g)
-	{
+	//public void paintComponent(Graphics g)
+	//{
 		
-		super.paintComponent(g);
+	//	super.paintComponent(g);
 		
+	//	int w = getWidth();
+	//	int h = getHeight();
 		
-	}
+	//}
 	
 }
-
